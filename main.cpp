979bee5ca2bdc85bff5613ae1080ddcac3cc454d
Ahmet Erdem <56089895+paraporo@users.noreply.github.com>
2023-01-12 17:18:10 +0300

    Add cd command

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <filesystem>
 
+namespace fs = std::filesystem;
 using namespace std;
 
 void getWords(string const& s, vector<string> &words)
@@ -27,12 +29,50 @@ int main()
 {
     string input;
     vector<string> words;
+    fs::path path("/"), prev(path), temp;
     while (true)
     {
+        cout << path << "$ ";
         getline(cin, input);
         getWords(input, words);
-        for (string word: words)
-            cout << word << " ";
-        cout << endl;
+        switch (words.size())
+        {
+            case 1:
+                if (words[0] == "cd")
+                {
+                    prev = path;
+                    path = "/";
+                }
+                break;
+            case 2:
+                if (words[0] == "cd")
+                {
+                    if (words[1] == "..")
+                    {
+                        prev = path;
+                        path = path.parent_path();
+                    }
+                    else if (words[1] == "-")
+                    {
+                        temp = prev;
+                        prev = path;
+                        path = temp;
+                    }
+                    else if (words[1] != ".")
+                    {
+                        temp = path;
+                        path.append(words[1]);
+                        if (fs::exists(path))
+                        {
+                            prev = temp;
+                        }
+                        else
+                        {
+                            path = temp;
+                            cout << "Incorrect path" << endl;
+                        }
+                    }
+                }
+        }
     }
 }
