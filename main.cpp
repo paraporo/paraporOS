f29f7f446c9ab4ab3e83740f36451c99cabac166
Ahmet Erdem <56089895+paraporo@users.noreply.github.com>
2023-01-13 03:08:21 +0300

    Add parseInput function

--- main.cpp
+++ main.cpp
@@ -32,23 +32,75 @@ void getWords(string const& s, vector<string> &words)
     }
 }
 
+void parseInput(string const& s, string &command, vector<string> &arguments, vector<string> &options)
+{
+    command.clear(), arguments.clear(), options.clear();
+    bool first(true), space(true);
+    for (char c: s)
+    {
+        if (c == ' ')
+        {
+            if (!space)
+                first = false;
+            space = true;
+        }
+        else
+        {
+            if (first)
+                command += c;
+            else
+            {
+                if (space)
+                    arguments.push_back("");
+                arguments[arguments.size() - 1] += c;
+            }
+            space = false;
+        }
+    }
+    for (int i(0); i < arguments.size(); i++)
+    {
+        if (arguments[i].size() >= 2 && arguments[i][0] == '-' && arguments[i][1] != '-')
+        {
+            for (int j(1); j < arguments[i].size(); j++)
+                options.push_back(string(1, arguments[i][j]));
+            arguments.erase(arguments.begin() + i);
+            i--;
+        }
+        else if (arguments[i].size() >= 3 && arguments[i][0] == '-' && arguments[i][1] == '-' && arguments[i][2] != '-')
+        {
+            options.push_back(arguments[i].substr(2, arguments[i].size() - 2));
+            arguments.erase(arguments.begin() + i);
+            i--;
+        }
+    }
+}
+
 int main()
 {
-    string input, output, home;
+    string input, output, home, shell("bash"), command;
     for (int i(0); homedir[i] != '\0'; i++)
         home += homedir[i];
-    vector<string> words, entries;
+    vector<string> words, entries, arguments, options;
     fs::path path(homedir), prev(path), temp;
-    string shell("bash");
     while (true)
     {
         output = ((path.native().size() >= home.size()) && (path.native().substr(0, home.size()) == home)) ? "~" + path.native().substr(home.size()) : path.native();
         if (shell == "zsh")
-            cout << "\033[0;44m  " << output<<" \033[0;34m \033[0m";
+            cout << "\033[0;44m  " << output <<" \033[0;34m \033[0m";
         else
             cout << "\033[1;34m" << output << "\033[0m$ ";
         getline(cin, input);
         getWords(input, words);
+        parseInput(input, command, arguments, options);
+        cout << "Command: " << command << endl;
+        cout << "Arguments: ";
+        for (int i(0); i < arguments.size(); i++)
+            cout << arguments[i] << " ";
+        cout << endl;
+        cout << "Options: ";
+        for (int i(0); i < options.size(); i++)
+            cout << options[i] << " ";
+        cout << endl;
         if (words.size() == 1)
         {
             if (words[0] == "cd")
@@ -70,9 +122,9 @@ int main()
                 {
                     for (int i(0); i < entries.size(); i++)
                     {
-                        cout << entries[i] << " ";
-                        if (!(i % 6))
+                        if (i && !(i % 6))
                             cout << endl;
+                        cout << entries[i] << " ";
                     }
                 }
                 cout << endl;
